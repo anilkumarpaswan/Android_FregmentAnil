a2ee167394adf87c0cc712b00779cc9d3b463615
ANIL KUMAR PASWAN <anilkumarpaswan928@gmail.com>
2021-11-27 00:21:38 +0530

    first commit

---
+++ app/src/main/java/com/example/andoid_fraregmentanil/Fragment2Anil.java
@@ -0,0 +1,64 @@
+package com.example.andoid_fraregmentanil;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Fragment2Anil#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment2Anil extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment2Anil() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Fragment2Anil.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Fragment2Anil newInstance(String param1, String param2) {
+        Fragment2Anil fragment = new Fragment2Anil ();
+        Bundle args = new Bundle ();
+        args.putString ( ARG_PARAM1, param1 );
+        args.putString ( ARG_PARAM2, param2 );
+        fragment.setArguments ( args );
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        if (getArguments () != null) {
+            mParam1 = getArguments ().getString ( ARG_PARAM1 );
+            mParam2 = getArguments ().getString ( ARG_PARAM2 );
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate ( R.layout.fragment_fragment2_anil, container, false );
+    }
+}
---
+++ app/src/main/java/com/example/andoid_fraregmentanil/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.andoid_fraregmentanil;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        onCreate ( R.layout.activity_main );
+
+    }
+
+    private void onCreate(int activity_main) {
+    }
+
+
+}
